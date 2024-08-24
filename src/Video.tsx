cbb9e4b097ead2766bd7f923a3e8145584a2dfc0
altcode <cosmosx1328@gmail.com>
2024-08-24 21:43:09 +0600

    refactor(Video): replace url with blob url

--- src/Video.tsx
+++ src/Video.tsx
@@ -1,18 +1,37 @@
-import { Accessor, Component } from 'solid-js';
-import { NoteView, NoteModel, Workspace } from "./model";
+import { Accessor, Component, createSignal, onCleanup } from 'solid-js';
+import axios from 'axios';
+import { NoteView, NoteModel, Range } from "./model";
 
 interface VideoProps {
   selectionRange: Accessor<Range>;
-	noteView: Accessor<NoteView>;
-	note: NoteModel;
+  noteView: Accessor<NoteView>;
+  note: NoteModel;
 }
 
 const Video: Component<VideoProps> = (props: VideoProps) => {
+  const [videoUrl, setVideoUrl] = createSignal<string | undefined>(undefined);
   const noteView = props.noteView;
+
+  const fetchVideo = async () => {
+    try {
+      const response = await axios.get(`http://localhost:80/api/files/${props.note.fileId}`, {
+        responseType: 'blob'
+      });
+      const blob = new Blob([response.data], { type: response.headers['content-type'] });
+      const url = URL.createObjectURL(blob);
+      setVideoUrl(url);
+
+    } catch (error) {
+      console.error('Error fetching video:', error);
+    }
+  };
+
+  onCleanup(() => URL.revokeObjectURL(videoUrl()));
+
+  fetchVideo();
 
   return (
     <video
-      src={`http://localhost:80/api/files/${props.note.fileId}`}
       width={noteView().width + "px"}
       height={noteView().height + "px"}
       style={{
@@ -20,9 +39,10 @@ const Video: Component<VideoProps> = (props: VideoProps) => {
         "max-height": "none",
         border: noteView().isSelected
         && props.selectionRange() === null ? "1px solid #ff4d00" : "none"
-      }} draggable="false">
+      }} draggable="false" controls>
+      {videoUrl() && <source src={videoUrl()} type="video/mp4"/>}
     </video>
-  )
-}
+  );
+};
 
 export default Video;
